2578ff7f5c7347644082a3634ef0e94d9d002a94
TatsuyoshiOya <Kozanchu8131@gmail.com>
2019-07-17 13:52:35 +0900

    チャプター2

---
+++ chap2.rb
@@ -0,0 +1,28 @@
+# 2-1
+puts 2+3
+puts 2*2*3.14
+
+# 2-2
+puts "Ruby"
+puts "abc" + "def"
+puts "123".to_i + "456".to_i
+
+# 2-3
+coffee = 300
+esplesso = 100
+puts "コーヒー:#{coffee}"
+puts "合計:#{coffee + esplesso * 2}"
+
+coffee = 400
+puts "コーヒー:#{coffee}"
+puts "合計:#{coffee + esplesso * 2}"
+
+# 2-6
+total = 300 * 2
+tax = total * 0.08
+p total
+puts tax
+
+# 2-7
+total = 300 * 2
+puts total
